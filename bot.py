754987109b4b2982e4cfc45d8853e0929b7989c7
Jackohhh <jackythejack11@gmail.com>
2018-06-18 12:56:08 +1200

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,6 +6,7 @@ description = '''Bot description here'''
 #bot = commands.Bot(command_prefix='>', description=description)
 client = discord.Client()
 
+errorChannel = 'unset'
 
 # Print the starting text
 print('---------------')
@@ -19,8 +20,17 @@ async def on_message(msg):
 
 @client.event
 async def on_ready():
+	for server in client.servers:
+		if server.id == '327604485386010636':
+			for channel in server.channels:
+				if channel.id == '455617398465363970':
+					errorChannel = channel
+					break
+			break
 	print('BOT LOADED')
+	print('Error logging channel: {}'.format(errorChannel.name))
 	await client.change_presence(game=discord.Game(name='Spotify',type=2),status=discord.Status.dnd)
+	await client.send_message(errorChannel,'I TOO HAVE BEEN REBORN!!!!!!')
 	return
 
 if __name__ == '__main__':
