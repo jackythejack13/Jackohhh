e8035d36bd38ba067e213a82d17ef46ba0c5dd3f
Jackohhh <jackythejack11@gmail.com>
2018-06-21 09:45:26 +1200

    Update misc.py

--- commands/misc.py
+++ commands/misc.py
@@ -3,5 +3,5 @@ from datetime import datetime
 
 async def ping(client,message):
   timestamp = message.timestamp.microsecond
-  newstamp = datetime.today().microsecond - timestamp
+  newstamp = round((datetime.today().microsecond - timestamp) / 1000,0)
   await client.send_message(message.channel,"PONG! `{}ms`".format(newstamp))
