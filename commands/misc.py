b2bc53d0355a5d89a71f496da6a210b7cb7c3b23
Jackohhh <jackythejack11@gmail.com>
2018-06-21 09:34:07 +1200

    Update misc.py

--- commands/misc.py
+++ commands/misc.py
@@ -2,5 +2,5 @@ import discord
 from datetime import datetime
 
 async def ping(client,message):
-  timestamp = message.timestamp.microseconds
+  timestamp = datetime.fromtimestamp(message.timestamp).microseconds
   await client.send_message(message.channel,"PONG! `{}ms`".format(datetime.time().microseconds))
