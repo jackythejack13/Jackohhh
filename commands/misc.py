ce10061b0f6e15238f62b0b76d1eaf1b2103246b
Jackohhh <jackythejack11@gmail.com>
2018-08-07 19:03:49 +1200

    Delete misc.py

--- commands/misc.py
+++
@@ -1,7 +0,0 @@
-import discord
-from datetime import datetime
-
-async def ping(client,message):
-  timestamp = message.timestamp.microsecond
-  newstamp = round((datetime.today().microsecond - timestamp) / 1000,0)
-  await client.send_message(message.channel,"PONG! `{}ms`".format(newstamp))
